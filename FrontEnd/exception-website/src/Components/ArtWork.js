65439303061d7557ff2cfae291ba2b5912a3bf77
Daelo Namaumbo <bsc-53-16@cc.ac.mw>
2022-08-03 13:23:39 -0700

    done artwork

--- FrontEnd/exception-website/src/Components/ArtWork.js
+++ FrontEnd/exception-website/src/Components/ArtWork.js
@@ -7,75 +7,79 @@ import photo3 from "../Asserts/photo3.jpg"
 export default function ArtWork() {
     return (
         <>
-            <Grid>
-                <Grid.Row columns={4}>
-                    <Grid.Column>
-                        <div style={{ "border": "1.3px solid #e0e1e2" }}>
-                            <Image src={photo1} alt="photo1" width="200px" />
-                        </div>
-                        <p>Playing- Austin Oaks</p>
-
-                        {/* buttons */}
-                        <Button icon labelPosition='left' color="orange" size="tiny">
-                            <Icon name='play' />
-                            PLAY
-                        </Button>
-
-                        <Button icon labelPosition='right' color="orange" size="tiny">
-                            DOWNLOAD NOW
-                            <Icon name='down arrow' />
-                        </Button>
-                    </Grid.Column>
-                    <Grid.Column>
-                        <div style={{ "border": "1.3px solid black" }}>
-                            <Image src={photo2} alt="photo3" width="200px" />
-                        </div>
-                        <p>Kings and Queens- Austin Oaks</p>
-                        {/* buttons */}
-                        <Button icon labelPosition='left' color="orange" size="tiny">
-                            <Icon name='play' />
-                            PLAY
-                        </Button>
-                        <Button icon labelPosition='right' color="orange" size="tiny">
-                            DOWNLOAD NOW
-                            <Icon name='down arrow' />
-                        </Button>
-                    </Grid.Column>
-                    <Grid.Column>
-                        <div style={{ "border": "1.3px solid black" }}>
-                            <Image src={photo1} alt="photo2" width="200px" />
-                        </div>
-                        <p>Christ- Austin Oaks</p>
-                        {/* buttons */}
-                        <Button icon labelPosition='left' color="orange" size="tiny">
-                            <Icon name='play' />
-                            PLAY
-                        </Button>
-                        <Button icon labelPosition='right' color="orange" size="tiny">
-                            DOWNLOAD NOW
-                            <Icon name='down arrow' />
-                        </Button>
-                    </Grid.Column>
-                    <Grid.Column>
-                        <div style={{ "border": "1.3px solid black","padding":"5px" }}>
-                            <Image src={photo3} alt="photo1" width="200px" hieght="200px" />
-
-                        </div>
-                        <p>Playing- Austin Oaks</p>
-
-                        {/* buttons */}
-                        <Button icon labelPosition='left' color="orange" size="tiny">
-                            <Icon name='play' />
-                            PLAY
-                        </Button>
-                        <Button icon labelPosition='right' color="orange" size="tiny">
-                            DOWNLOAD NOW
-                            <Icon name='down arrow' />
-                        </Button>
-                    </Grid.Column>
-                </Grid.Row>
-            </Grid>
-            <hr />
+            <div >
+                <Grid centered="true" container="true">
+                    <Grid.Row columns={4} >
+                        <Grid.Column>
+                            <div style={{ "border": "15px solid  #e0e1e2", "borderRadius": "20px", "width": "200px", "padding": "5px" }}>
+                                <Image src={photo1} alt="photo1" width="200px" />
+                                
+                            </div>
+                            <p>Playing- Austin Oaks</p>
+                        
+                                {/* buttons */}
+                                <Button icon labelPosition='left' color="orange" size="mini">
+                                    <Icon name='play' />
+                                    PLAY
+                                </Button>
+                                <Button icon labelPosition='right' color="orange" size="mini">
+                                    DOWNLOAD NOW
+                                    <Icon name='down arrow' />
+                                </Button>
+                            
+                        </Grid.Column>
+                        <Grid.Column>
+                            <div style={{ "border": "15px solid  #e0e1e2", "borderRadius": "20px", "width": "200px", "padding": "5px" }}>
+                                <Image src={photo2} alt="photo3" width="200px" />
+                            </div>
+                            <p>Kings and Queens- Austin Oaks</p>
+                            {/* buttons */}
+                            <Button icon labelPosition='left' color="orange" size="mini">
+                                <Icon name='play' />
+                                PLAY
+                            </Button>
+                            <Button icon labelPosition='right' color="orange"  size="mini">
+                                DOWNLOAD NOW
+                                <Icon name='down arrow' />
+                            </Button>
+                        </Grid.Column>
+                        <Grid.Column>
+                            <div style={{ "border": "15px solid  #e0e1e2", "borderRadius": "20px", "width": "200px", "padding": "5px" }}>
+                                <Image src={photo1} alt="photo2" width="200px" />
+                            </div>
+                            <p>Christ- Austin Oaks</p>
+                            {/* buttons */}
+                            <Button icon labelPosition='left' color="orange"  size="mini">
+                                <Icon name='play' />
+                                PLAY
+                            </Button>
+                            <Button icon labelPosition='right' color="orange"  size="mini">
+                                DOWNLOAD NOW
+                                <Icon name='down arrow' />
+                            </Button>
+                        </Grid.Column>
+                        <Grid.Column>
+                            <div style={{ "border": "15px solid  #e0e1e2", "borderRadius": "20px", "width": "200px", "padding": "5px" }}>
+                                <Image src={photo3} alt="photo1" width="200px" hieght="200px" />
+                            </div>
+                            <p>Playing- Austin Oaks</p>
+                            {/* buttons */}
+                            <Button icon labelPosition='left' color="orange"  size="mini">
+                                <Icon name='play' />
+                                PLAY
+                            </Button>
+                            <Button icon labelPosition='right' color="orange"  size="mini">
+                                DOWNLOAD NOW
+                                <Icon name='down arrow' />
+                            </Button>
+                        </Grid.Column>
+                    </Grid.Row>
+                </Grid>
+                <hr style={{
+                    "marginLeft": "150px",
+                    "marginRight": "150px"
+                }} />
+            </div>
         </>
     )
 }
