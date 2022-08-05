45ddb6646dee9e7951c90ae43767ed5220b19957
Daelo Namaumbo <bsc-53-16@cc.ac.mw>
2022-08-05 01:30:37 -0700

    playing working

--- FrontEnd/exception-website/src/Components/Playing.js
+++ FrontEnd/exception-website/src/Components/Playing.js
@@ -1,25 +1,32 @@
 import React from "react";
-import { Image, Segment, Grid, Button, Header } from "semantic-ui-react";
+import { Image, Segment, Grid, Button, Header, Icon ,Progress} from "semantic-ui-react";
 import photo1 from "../Asserts/photo1.jpg";
 
 const Playing = () => (
   <>
-  <Segment>
-    <Grid centered="true" container="true">
-    <Grid.Row columns={2}>
-    <Grid.Column>
-        <Button color="orange" compact>NOW PLAYING</Button>
-        <br />
-        <Header size="huge" style={{fontSize:"45px"}}>Playing</Header>
-        <Header size="large">Qone & Servant</Header>
-        <Header size="medium">2022</Header>
-    </Grid.Column> 
+    <Segment>
+      <Grid centered="true" container="true">
+        <Grid.Row columns={2}>
+          <Grid.Column>
+            <Button color="orange" compact>
+              NOW PLAYING
+            </Button>
+            <br />
+            <Header size="huge" style={{ fontSize: "45px" }}>
+              Playing
+            </Header>
+            <Header size="large">Qone & Servant</Header>
+            <Header size="medium">(2022)</Header>
+            <Icon name="pause" size="big"></Icon>
+            <Progress percent={78} size="tiny">
     
-    <Grid.Column>    
-        <Image src={photo1} width="230px" circular />
-    </Grid.Column>
-      </Grid.Row>
-    </Grid>
+            </Progress>
+          </Grid.Column>
+          <Grid.Column>
+            <Image src={photo1} width="230px" circular />
+          </Grid.Column>
+        </Grid.Row>
+      </Grid>
     </Segment>
   </>
 );
