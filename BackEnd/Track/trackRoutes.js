a9c0d9d5863432304597df7c03a4bd6d14f6de32
Daelo Namaumbo <bsc-53-16@cc.ac.mw>
2022-10-20 14:25:43 +0200

    api-excepcion

--- BackEnd/Track/trackRoutes.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const songsController = require("./TrackController");
-
-router.get("/get-tracks", songsController.listFiles);
-router.get('/get-track/:title',songsController.findTrack)
-module.exports = router;
